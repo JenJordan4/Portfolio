808d2793e3a144a85e7a0766ea37908c9fca88f0
jenjordan4 <jbaldwin4@hotmail.com>
2019-12-16 18:47:25 -0500

    laying groundwork for react routers

---
+++ client/src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <main className="about-main">
+      <h1>I'm Jen Jordan</h1>
+      <p>Helping others and challenging myself has always been the core values of who I am. 
+        These values has brought me everywhere from teaching customers how to use their new 
+        iPhones, insipiring the next generation to love literature, and now as a full-stack web developer.
+      </p>
+    </main>
+  )
+}
+    
+export default About
---
+++ client/src/pages/Home.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import selfie from "../images/selfie.jpg"
+import '../App.css'
+
+const Home = () => {
+
+  return (
+    <>
+      <main className="home-main">
+        <h1>Hello, World!</h1>
+        <h2>I'm Jen Jordan.</h2>
+        <img id="profile-pic" src={ selfie } alt="This is me!"/>
+        <p>
+          I am a full-stack web development grad from 
+          <a href="https://www.wyncode.co" target="_blank">
+          Wyncode Academy
+          </a>!
+        </p>
+        <div>
+          <p>Some of my favorite programming languages are</p>
+          <ul id="language-list">
+              <li>
+                  <h3>CSS</h3>
+                  <img src="css.svg" alt="Cascading Style Sheets" id="css"/>
+              </li>
+              <li>
+                  <h3>JavaScript</h3>
+                  <img src="js.png" alt="JavaScript" />
+              </li>
+              <li>
+                  <h3>Ruby</h3>
+                  <img src="ruby.png" alt="Ruby" />
+              </li>
+          </ul>
+        </div>
+        <div>
+            <h2></h2>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default Home
